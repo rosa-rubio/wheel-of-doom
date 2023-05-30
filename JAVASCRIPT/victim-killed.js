e7d84d6473ac2bc36a6e266a4c4fdf40b743337e
florienborg <flornbg014@gmail.com>
2023-05-30 16:05:13 +0200

    Pag3

--- JAVASCRIPT/victim-killed.js
+++ JAVASCRIPT/victim-killed.js
@@ -1,2 +1,9 @@
 const nameArray = localStorage.getItem('nameArray');
 let names = nameArray ? JSON.parse(nameArray) : [];
+const btnReset = document.getElementById('next-victim-button')
+
+btnReset.addEventListener('click', clearMemory);
+
+function clearMemory() {
+    localStorage.removeItem(nameArray);
+}
