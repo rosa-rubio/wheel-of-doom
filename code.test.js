22fc7949c7b648efaa64456a44477c720aa28d96
florienborg <flornbg014@gmail.com>
2023-06-01 14:47:46 +0200

    intento de testing

---
+++ code.test.js
@@ -0,0 +1,24 @@
+//const = require();
+
+    //describe ('Funcionalidad Boton Ingresar al Juego', () => {
+  //      test('Al hacer click, el botón nos redirige a la página 2', () => {
+         //   expect().ToBe();
+        //  })
+    // });
+
+
+
+    //TEST INTEGRADOR. PREFERIBLE HACER UN TEST UNITARIO PRIMERO
+
+//const { killButton, killSound} = require('./javascript/select-gamers');
+
+//describe('Funcionalidad del Boton Pulsa para Matar', () => {
+//    test('Redirige al Pop-up después del sonido', (done) => {
+ //     killButton.dispatchEvent(new window.Event('click'));
+
+ //     setTimeout(() => {
+  //      expect(window.location.href).toBe('./javascript/pop-up.js');
+  //      done();
+   //   }, killSound.duration * 1000);
+  //  });
+ // });
--- javascript/start-game.js
+++ javascript/start-game.js
@@ -1,3 +1,18 @@
+//Boton Ingresar al Juego // 
+const accessButton = document.querySelector("#access-button");
+const knifeSound = new Audio("/Sounds/slashkut.mp3");
+
+accessButton.addEventListener("click", function(event) {
+    knifeSound.currentTime = 0;
+    knifeSound.play();
+
+setTimeout(function(redirectToPage) { 
+  window.location.href = "./select-gamers.html";
+  }, knifeSound.duration * 1000);
+
+  event.preventDefault();
+});
+
 //Reproductor de música con botón de pause/play
 
 const musicPlayer = document.getElementById('music-player');
@@ -13,18 +28,5 @@ buttonPausePlay.addEventListener('click', function() {
   }
 });
 
-//Boton Ingresar al Juego // 
-const accessButton = document.querySelector("#access-button");
-const knifeSound = new Audio("/Sounds/slashkut.mp3");
 
-accessButton.addEventListener("click", function(event) {
-    knifeSound.currentTime = 0;
-    knifeSound.play();
-
-setTimeout(function(redirectToPage) { 
-  window.location.href = "./select-gamers.html";
-  }, knifeSound.duration * 1000);
-
-  event.preventDefault();
-});
 
